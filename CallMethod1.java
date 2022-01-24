381aed2bb08ed274b95a8e57e9f6ad17f91bfca3
Harshita <mailmeharshita24@gmail.com>
2022-01-24 14:26:28 +0530

    COMMIT

---
+++ CallMethod1.java
@@ -0,0 +1,20 @@
+
+public class CallMethod1 {
+	
+	public static void myDog() {
+		System.out.println("My Dog name iS Fifi!!");
+	}
+	
+	void myCat() {
+		System.out.println("My Cat is fluffy! ");
+	}
+
+	public static void main(String[] args) {
+		
+		myDog();
+		new CallMethod1().myCat();
+		
+
+	}
+
+}
