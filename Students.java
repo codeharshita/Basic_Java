0dbf53d88be56d6bf053d1e8f3cc412bac1b6994
Harshita <mailmeharshita24@gmail.com>
2022-01-26 20:40:14 +0530

    New Commit

---
+++ Students.java
@@ -0,0 +1,17 @@
+package ListConcept;
+
+public class Students {
+
+	String name;
+	int id;
+	String gender;
+	int num;
+	
+	Students(String name, int id, String gender, int num){
+		this.name= name;
+		this.id=id;
+		this.gender =gender;
+		this.num=num;
+		}
+	
+	}
