c0cbc3745fe259382dda38d820a08e99ab7854b4
Harshita <mailmeharshita24@gmail.com>
2022-02-05 20:25:25 +0530

    New Commit

---
+++ StringConcatenation.java
@@ -0,0 +1,24 @@
+package JavaBasics;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j=52;
+		
+		String x="Hello";
+		String y="World";
+		
+		System.out.println(i+j);
+		
+		System.out.println(i+j+x+y);
+		
+		System.out.println(x+y+i+j);
+		
+		System.out.println(x+y+(i+j));
+		
+		
+
+	}
+
+}
