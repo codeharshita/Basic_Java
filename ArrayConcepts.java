6e3698651c562151c155b41fc0fc4bc26689524a
Harshita <mailmeharshita24@gmail.com>
2022-02-07 09:16:29 +0530

    New Commit

---
+++ ArrayConcepts.java
@@ -0,0 +1,23 @@
+package JavaBasics;
+
+public class ArrayConcepts {
+
+	public static void main(String[] args) {
+		
+		
+		int i[]= new int [4];
+		
+		i[0]=1;
+		i[1]=6;
+		i[2]=7;
+		i[3]=3;
+		
+		System.out.println(i.length);
+		
+		for(int j =0;j<i.length;j++) {
+			System.out.println(i[j]);
+		}
+
+	}
+
+}
